c2bc9c05e84e311f3853a3a54f85886a85992567
VYSAKRAJ <vysak@phyction.co>
2020-03-30 10:56:09 +0530

    comm

--- YTVDownloader/views.py
+++ YTVDownloader/views.py
@@ -1,27 +1,26 @@
 import pafy
+import urllib.request
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template.defaultfilters import filesizeformat
+import urllib.request
 
 from .forms import DownloadForm
 
-def download_video(request):
-    global context
-    form = DownloadForm(request.POST or None)
-    if form.is_valid():
-        video_url = form.cleaned_data.get("url")
-        # if 'm.' in video_url:
-        #     video_url = video_url.replace(u'm.', u'')
-        #
-        # elif 'youtu.be' in video_url:
-        #     video_id = video_url.split('/')[-1]
-        #     video_url = 'https://www.youtube.com/watch?v=' + video_id
-        #
-        # if len(video_url.split("=")[-1]) != 11:
-        #     return HttpResponse('Enter correct url.')
-        print(video_url)
+
+class parser:
+    def __init__(self, url):
+        self.url = url
+        self.context=None
+        self.Download_return()
+
+    def getData(self):# return full dataframe from file
+        return (self.context)
+
+    def Download_return(self):
+        # if urllib.request.urlopen(self.url).code == 200:
         pafy.set_api_key('AIzaSyCp0RX2L3eDzuNiPNJr34jom1-gUsj_ze0')
-        video = pafy.new(video_url)
+        video = pafy.new(self.url)
         stream = video.streams
         print (video.audiostreams)
         video_audio_streams = []
@@ -29,7 +28,7 @@ def download_video(request):
             video_audio_streams.append({
                 'resolution': s.resolution,
                 'extension': s.extension,
-                'file_size': filesizeformat(s.get_filesize()),
+                # 'file_size': filesizeformat(s.get_filesize()),
                 'video_url': s.url + "&title=" + video.title
             })
 
@@ -56,14 +55,37 @@ def download_video(request):
             })
         print('before context')
         context = {
-            'form': form,
+            # 'form': form,
             'title': video.title, 'streams': video_audio_streams,
             'description': video.description, 'likes': video.likes,
             'dislikes': video.dislikes, 'thumb': video.bigthumbhd,
             'duration': video.duration, 'views': video.viewcount,
             'stream_video': video_streams, 'stream_audio': audio_streams
         }
-        print('after context')
-        return render(request, 'home.html', context)
+        self.context=context
+        return
+
+
+
+
+def download_video(request):
+    global context
+    form = DownloadForm(request.POST or None)
+    if form.is_valid():
+        video_url = form.cleaned_data.get("url")
+        # if 'm.' in video_url:
+        #     video_url = video_url.replace(u'm.', u'')
+        #
+        # elif 'youtu.be' in video_url:
+        #     video_id = video_url.split('/')[-1]
+        #     video_url = 'https://www.youtube.com/watch?v=' + video_id
+        #
+        # if len(video_url.split("=")[-1]) != 11:
+        #     return HttpResponse('Enter correct url.')
+        obj1=parser(video_url)
+        context=obj1.getData()
+        print(context)
+        return render(request, 'home.html',context )
 
     return render(request, 'home.html', { 'form': form })
+
---
+++ test.py
@@ -0,0 +1,4 @@
+
+import pafy
+v = pafy.new("https://youtu.be/zADj0k0waFY")
+print(v.audiostreams)
